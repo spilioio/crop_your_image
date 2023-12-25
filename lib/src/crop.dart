bc38b3efdd96f91ae71888cd0d727a29e865beba
Alex Consel <alex.consel@gmail.com>
2023-12-25 15:40:31 +0100

    optional base color

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -457,6 +457,7 @@ class _CropEditorState extends State<_CropEditor> {
                   onScaleStart: widget.interactive ? _startScale : null,
                   onScaleUpdate: widget.interactive ? _updateScale : null,
                   child: Container(
+                    color: widget.baseColor,
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.height,
                     child: Stack(
